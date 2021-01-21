f116827a42c1886d2a19173ef2c0efb56f1100d0
asherphil <77658420+asherphil@users.noreply.github.com>
2021-01-21 10:28:39 -0500

    Add files via upload

--- derivative1.py
+++ derivative1.py
@@ -1,8 +1,33 @@
-coeffstr = input("what is your coefficient")
-powerstr = input("what is your power")
-coeffint = int(coeffstr)
-powerint = int(coeffstr)
-new_coeffstr = str(coeffint*powerint)
-new_powerstr = str(powerint-1)
-print("intial function:"+coeffstr+"x^"+powerstr)
-print("derivative:"+new_coeffstr+"x^"+new_powerstr)
+def getfunc():
+    coeffstr = input("what is your coefficient")
+    powerstr = input("what is your power")
+    print("Initial function:" + coeffstr + "x^" + powerstr)
+    return (coeffstr,powerstr)
+def getop():
+    op = input("For derivative, tpye d. For Intergal type i")
+    if op == 'd':
+        return 0
+    elif op == 'i':
+        return 1
+    else: 
+        print("impropper response")
+def derivative(coeff, power): 
+    coeffint = float(coeff)
+    powerint = float(power)
+    new_coeffstr = str(coeffint*powerint)
+    new_powerstr = str(powerint-1)
+    print("derivative:"+new_coeffstr+"x^"+new_powerstr)  
+def intergal(coeff, power):
+    coeffint = float(coeff)
+    powerint = float(power)
+    new_powerstr = str(powerint+1)
+    new_coeffstr = str(coeffint/(powerint+1))
+    print("Integral:"+new_coeffstr+"x^"+new_powerstr)
+    
+nums = getfunc()
+opan = getop()
+if opan == 0:
+    derivative(nums[0], nums[1])
+elif opan == 1:
+    intergal(nums[0], nums[1])
+
