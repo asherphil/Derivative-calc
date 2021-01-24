ff82e7d906915602cf03ece806ed70b8d8ac5fc7
asherphil <77658420+asherphil@users.noreply.github.com>
2021-01-24 00:36:52 -0500

    Add files via upload

--- derivative1.py
+++ derivative1.py
@@ -1,8 +1,14 @@
 def getfunc():
-    coeffstr = input("what is your coefficient")
-    powerstr = input("what is your power")
-    print("Initial function:" + coeffstr + "x^" + powerstr)
-    return (coeffstr,powerstr)
+   func= input("what is you function")
+   func2 = func.split('+')
+   print(func2)  
+   numlist=[]
+   for x in func2:
+       print(x)
+       u= (x.split('x^'))
+       numlist.append(u)
+   print(numlist)
+   return (numlist)
 def getop():
     op = input("For derivative, tpye d. For Intergal type i")
     if op == 'd':
@@ -11,23 +17,36 @@ def getop():
         return 1
     else: 
         print("impropper response")
-def derivative(coeff, power): 
-    coeffint = float(coeff)
-    powerint = float(power)
-    new_coeffstr = str(coeffint*powerint)
-    new_powerstr = str(powerint-1)
-    print("derivative:"+new_coeffstr+"x^"+new_powerstr)  
+def derivative(val): 
+    newnumlist=[]
+    for x in val:
+        coef = int(x[0]) * int(x[1])
+        pwr = int(x[1])-1
+        newnumlist.append([coef,pwr])
+    print(newnumlist)
+    return newnumlist
+            
+        
+    
 def intergal(coeff, power):
     coeffint = float(coeff)
     powerint = float(power)
     new_powerstr = str(powerint+1)
     new_coeffstr = str(coeffint/(powerint+1))
     print("Integral:"+new_coeffstr+"x^"+new_powerstr)
-    
+
+def together(val2):
+    monobuild=[]
+    for x in val2:
+        monobuild.append(str(x[0]) + 'x^' + str(x[1]))
+    print('+'.join(monobuild))
+      
+   
 nums = getfunc()
 opan = getop()
 if opan == 0:
-    derivative(nums[0], nums[1])
+    butt= derivative(nums)
+    together(butt)
 elif opan == 1:
     intergal(nums[0], nums[1])
 
